b5eff44e2da84e583805781701be76a526a1eb6b
maxim <uzumaxy@gmail.com>
2015-07-16 21:17:26 +0300

    Autopublish all useful info about all users.

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,3 +1,12 @@
 Meteor.publish('messages', function () {
   return Messages.find();
 });
+
+Meteor.publish(null, function () {
+  return Meteor.users.find({}, {
+    fields: {
+      username: 1,
+      profile: 1
+    }
+  });
+});
